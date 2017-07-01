6686c4b2727bbbd647223b4efddf68d9eac9a65b
Chanpreet08 <chanpreet.chhabra@gmail.com>
2017-07-01 19:12:04 +0530

    socket added

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
-var server = require('http').server(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var setup = require('./routes/setup');
+var msg = require('./routes/msg');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,6 +29,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.use('/setup', setup);
+app.use('/msg',msg);
+
+
+io.on('connection', function(socket) {
+  //Globals
+  var defaultRoom = 'general';
+  var rooms = ["General", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
+
+  socket.emit('setup', {
+    rooms: rooms
+  });
+
+  
+  socket.on('new user', function(data) {
+    data.room = defaultRoom;
+    
+    socket.join(defaultRoom);
+    
+    io.in(defaultRoom).emit('user joined', data);
+  });
+
+  //Listens for switch room
+  socket.on('switch room', function(data) {
+    
+    socket.leave(data.oldRoom);
+    socket.join(data.newRoom);
+    io.in(data.oldRoom).emit('user left', data);
+    io.in(data.newRoom).emit('user joined', data);
+
+  });
+
+  
+  socket.on('new message', function(data) {
+    
+    var newMsg = new Chat({
+      username: data.username,
+      content: data.message,
+      room: data.room.toLowerCase(),
+      created: new Date()
+    });
+    
+        newMsg.save(function(err, msg){
+      
+      io.in(msg.room).emit('message created', msg);
+    });
+  });
+});
+
+server.listen(2015);
+console.log('It\'s going down in 2015');
 
 app.all('*',function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
@@ -39,17 +91,14 @@ app.all('*',function(req,res,next){
     next();
   }
 })
-/// catch 404 and forwarding to error handler
+
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/// error handlers
 
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
