2587050529ea8038c6aa83d83ceb0e948f10fe19
Chanpreet08 <chanpreet.chhabra@gmail.com>
2017-07-14 22:07:41 +0530

    new app

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
@@ -8,23 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var setup = require('./routes/setup');
-var msg = require('./routes/msg');
-var url = require('./config/dbconfig');
-// view engine setup
 
-mongoose.connect(url.url);
-var db = mongoose.connection;
-db.on('error',console.error.bind('connection to server is not established'));
-db.on('open',function(){
-    console.log('conection to database is established');
-})
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -35,115 +23,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/setup', setup);
-app.use('/msg',msg);
 
-// io.on('connection',(socket=>{
-//   console.log('a user is connected');
-
-//   socket.on('message',(data)=>{
-//       console.log('message='+data);
-//   });
-
-
-// }))
-
-// server.listen(3005,function(){
-//   console.log('listening on 3005');
-// });
-
-
-// app.use('/setup', setup);
-// app.use('/msg',msg);
-
-
-// io.on('connection',function(socket){
-//     console.log('vnjf');
-//     var deafultRoom = 'general';
-
-//     var rooms = ['General','angular','socket.io','express','node','mongo','php','laravel'];
-//     socket.emit('setup',{
-//         rooms:rooms
-//     });
-// })
-
-// server.listen(2015,function(){
-//     console.log('listening to 2015');
-// })
-// io.on('connection', function(socket) {
-//   //Globals
-//   var defaultRoom = 'general';
-//   var rooms = ["General", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
-
-//   socket.emit('setup', {
-//     rooms: rooms
-//   });
-
-  
-//   socket.on('new user', function(data) {
-//     data.room = defaultRoom;
-    
-//     socket.join(defaultRoom);
-    
-//     io.in(defaultRoom).emit('user joined', data);
-//   });
-
-//   //Listens for switch room
-//   socket.on('switch room', function(data) {
-    
-//     socket.leave(data.oldRoom);
-//     socket.join(data.newRoom);
-//     io.in(data.oldRoom).emit('user left', data);
-//     io.in(data.newRoom).emit('user joined', data);
-
-//   });
-
-  
-//   socket.on('new message', function(data) {
-    
-//     var newMsg = new Chat({
-//       username: data.username,
-//       content: data.message,
-//       room: data.room.toLowerCase(),
-//       created: new Date()
-//     });
-    
-//         newMsg.save(function(err, msg){
-      
-//       io.in(msg.room).emit('message created', msg);
-//     });
-//   });
-// });
-
-// server.listen(2015);
-// console.log('It\'s going down in 2015');
-
-// app.all('*',function(req,res,next){
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-//    if (req.method == 'OPTIONS') {
-//     res.status(200).end();
-//   } else {
-//     next();
-//   }
-// })
-
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
--- config/dbconfig.js
+++
@@ -1,6 +0,0 @@
-
-var dbusername = process.env.USER_NAME;
-var dbpassword = process.env.USER_PASSWORD;
-var dburl = process.env.DB_URL;
-
-exports.url = 'mongodb://'+dbusername+":"+dbpassword+dburl;
